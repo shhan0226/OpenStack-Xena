f92787b5131b87dcefe083957099c059214aff44
shhan <shhan0226@gmail.com>
2022-03-29 11:03:18 +0900

    inpute value

--- compute-release/start.sh
+++ compute-release/start.sh
@@ -2,6 +2,13 @@
 
 echo "Install Controller for OpenStack ..."
 
+# Inpute Value
+H_NAMEv="compute1"
+SET_IPv="192.168.1.5"
+SET_IP2v="192.168.1.6"
+SET_IP_ALLOWv="192.168.0.0/22"
+INTERFACE_NAME_v="eth0"
+STACK_PASSWDv="stack"
 
 read -p "Do you want to input ?? {yes|no|ENTER=no} :" CHECKER_O_
 if [ "$CHECKER_O_" = "yes" ]; then
@@ -18,14 +25,15 @@ if [ "$CHECKER_O_" = "yes" ]; then
     export INTERFACE_NAME_=INTERFACE_NAME_v
     export STACK_PASSWD=STACK_PASSWDv
 else    
-    export H_NAME="compute1"
-    export SET_IP="192.168.1.5"
-    export SET_IP2="192.168.1.6"
-    export SET_IP_ALLOW="192.168.0.0/22"
-    export INTERFACE_NAME_="eth0"
-    export STACK_PASSWD="stack"
+    export H_NAME=H_NAMEv
+    export SET_IP=SET_IPv
+    export SET_IP2=SET_IP2v
+    export SET_IP_ALLOW=SET_IP_ALLOWv
+    export INTERFACE_NAME_=INTERFACE_NAME_v
+    export STACK_PASSWD=STACK_PASSWDv
 fi
 
+# INPUT DATA PRINT
 echo "$H_NAME"
 echo "$SET_IP"
 echo "$SET_IP2"
@@ -34,9 +42,9 @@ echo "$INTERFACE_NAME_"
 echo "$STACK_PASSWD"
 echo "... set!!"
 
+# INSTALL START
 echo "1. Install Compute Setting ..."
 source ./services/compute_setting.sh
-
 read -p "Install Nova ?? {yes|no|ENTER=yes} :" CHECKER_Node
 if [ "$CHECKER_Node" = "no" ]; then
     echo "Please Install Contoller Node (Nova)"
@@ -45,7 +53,6 @@ else
     echo "2. Install Nova ..."
     source ./services/nova_compute.sh
 fi
-
 read -p "Install Neutron ?? {yes|no|ENTER=yes} :" CHECKER_Node
 if [ "$CHECKER_Node" = "no" ]; then
     echo "Please Install Contoller Node (Neutron)"
--- controller-release/start.sh
+++ controller-release/start.sh
@@ -2,6 +2,13 @@
 
 echo "Install Controller for OpenStack ..."
 
+# Inpute Value
+H_NAMEv="controller"
+SET_IPv="192.168.1.5"
+SET_IP2v="192.168.1.6"
+SET_IP_ALLOWv="192.168.0.0/22"
+INTERFACE_NAME_v="eth0"
+STACK_PASSWDv="stack"
 
 read -p "Do you want to input ?? {yes|no|ENTER=no}" CHECKER_O_
 if [ "$CHECKER_O_" = "yes" ]; then
@@ -18,14 +25,15 @@ if [ "$CHECKER_O_" = "yes" ]; then
     export INTERFACE_NAME_=INTERFACE_NAME_v
     export STACK_PASSWD=STACK_PASSWDv
 else
-    export H_NAME="controller"
-    export SET_IP="192.168.1.5"
-    export SET_IP2="192.168.1.6"
-    export SET_IP_ALLOW="192.168.0.0/22"
-    export INTERFACE_NAME_="eth0"
-    export STACK_PASSWD="stack"  
+    export H_NAME=H_NAMEv
+    export SET_IP=SET_IPv
+    export SET_IP2=SET_IP2v
+    export SET_IP_ALLOW=SET_IP_ALLOWv
+    export INTERFACE_NAME_=INTERFACE_NAME_v
+    export STACK_PASSWD=STACK_PASSWDv
 fi
 
+# INPUT DATA PRINT
 echo "$H_NAME"
 echo "$SET_IP"
 echo "$SET_IP2"
@@ -34,21 +42,17 @@ echo "$INTERFACE_NAME_"
 echo "$STACK_PASSWD"
 echo "... set!!"
 
+# INSTALL START
 echo "1. Install Controller Setting ..."
 source ./services/controller-setting.sh
-
 echo "2. Install Keystone ..."
 source ./services/keystone.sh
-
 echo "3. Install Glance ..."
 source ./services/glance.sh
-
 echo "4. Install Placement ..."
 source ./services/placement.sh
-
 echo "5. Install Nova ..."
 source ./services/nova.sh
-
 echo "Compelete, Nova!!"
 read -p "Install Compute Node ?? {yes|no|ENTER=yes} :" CHECKER_Node
 if [ "$CHECKER_Node" = "no" ]; then
@@ -57,10 +61,8 @@ else
     echo "6. Check Compute Node!!"
     source ./services/nova_check.sh    
 fi
-
 echo "7. Install Neutron ..."
 source ./services/neutron.sh
-
 echo "8. Install Horizon ..."
 source ./services/horizon.sh
 
