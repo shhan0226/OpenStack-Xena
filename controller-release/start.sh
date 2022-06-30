5f33d605583631bbe3671d9a54dc6b4128e80fd6
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2022-06-30 11:33:07 +0900

    Update HOST_NAME

--- controller-release/start.sh
+++ controller-release/start.sh
@@ -4,6 +4,7 @@ echo "Install Controller for OpenStack ..."
 
 # Inpute Value
 CONTROLLER_HOSTv="controller"
+COMPUTE_HOSTv="compute1"
 SET_IPv="192.168.1.5"
 SET_IP2v="192.168.1.6"
 SET_IP_ALLOWv="192.168.0.0/22"
@@ -12,13 +13,15 @@ STACK_PASSWDv="stack"
 
 read -p "Do you want to input ?? {yes|no|ENTER=no}" CHECKER_O_
 if [ "$CHECKER_O_" = "yes" ]; then
-    read -p "Input HOSTNAME: " CONTROLLER_HOSTv
+    read -p "Input CONTROLLER HOSTNAME: " CONTROLLER_HOSTv
+    read -p "Input COMPUTE HOSTNAME: " CONTROLLER_HOSTv
     read -p "Input Contorller IP: (ex.192.168.0.2) " SET_IPv
     read -p "Input Compute1 IP: (ex.192.168.0.3) " SET_IP2v
     read -p "Input the allow IP (ex 192.168.0.0/24): " SET_IP_ALLOWv
     read -p "Input INTERFACE_NAME: " INTERFACE_NAME_v
     read -p "Input STACK_PASSWD: " STACK_PASSWDv
     export CONTROLLER_HOST=$CONTROLLER_HOSTv
+    export COMPUTE_HOST=$COMPUTE_HOSTv
     export SET_IP=$SET_IPv
     export SET_IP2=$SET_IP2v
     export SET_IP_ALLOW=$SET_IP_ALLOWv
@@ -26,6 +29,7 @@ if [ "$CHECKER_O_" = "yes" ]; then
     export STACK_PASSWD=$STACK_PASSWDv
 else
     export CONTROLLER_HOST=$CONTROLLER_HOSTv
+    export COMPUTE_HOST=$COMPUTE_HOSTv
     export SET_IP=$SET_IPv
     export SET_IP2=$SET_IP2v
     export SET_IP_ALLOW=$SET_IP_ALLOWv
@@ -35,6 +39,7 @@ fi
 
 # INPUT DATA PRINT
 echo "$CONTROLLER_HOST"
+echo "$COMPUTE_HOST"
 echo "$SET_IP"
 echo "$SET_IP2"
 echo "$SET_IP_ALLOW"
